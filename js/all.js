0b5433d5ae649b362635a74eb261b9fc1161e21e
as28652865g <58939150+as28652865g@users.noreply.github.com>
2020-12-15 23:28:22 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,6 @@
+function toggle(){
+    var toggle = document.querySelector('.toggle');
+    var banner = document.querySelector('.banner');
+    toggle.classList.toggle('active')
+    banner.classList.toggle('active')
+}
